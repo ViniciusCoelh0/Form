2b5d114ca5b5aa1e68db06c74911e5db18a45bd9
Vinicius Coelho <vinicius.dantas.336@gmail.com>
2024-08-07 12:27:15 -0300

    form-validated

---
+++ js/validation.js
@@ -0,0 +1,43 @@
+function validateForm() {
+    var fName = document.getElementById("first").value;
+    var SName = document.getElementById("second").value;
+    var mail = document.getElementById("mail").value;
+    var querys = document.querySelector('input[name="drone"]:checked');
+    var checkBox = document.querySelector('input[name="confirm"]:checked')
+
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Check the name and last name
+    if (fName === "" || SName === "") {
+        alert("Please enter a valid first name and last name.");
+        return false;
+    }
+
+    // Test email validation
+    if(!(emailPattern.test(mail))){
+        alert("Please enter a valid email address.  ");
+        return false;
+    }
+
+    // Check the query selector
+    if(querys === null){
+        alert("Please select a query type.");
+        return false;
+    }
+
+    // Confirm checkBox
+    if(checkBox === null){
+        alert("Please select the chackbox.");
+        return false;
+    }
+
+    return true;
+
+}
+
+console.log(mail)
+
+// function validadeForm(){
+//     let form = document.forms[]
+// }
+
