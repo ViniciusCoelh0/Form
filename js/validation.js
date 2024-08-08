f26bc2a793398b061b3de1cef8dc7a99675273a8
Vinicius Coelho <vinicius.dantas.336@gmail.com>
2024-08-08 14:28:56 -0300

    Form-updated

--- js/validation.js
+++ js/validation.js
@@ -6,28 +6,35 @@ function validateForm() {
     var checkBox = document.querySelector('input[name="confirm"]:checked')
 
     var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var namePattern = /^[A-Za-zÀ-ÖØ-öø-ÿ]+(?:[ '-][A-Za-zÀ-ÖØ-öø-ÿ]+)*$/;
+
 
     // Check the name and last name
     if (fName === "" || SName === "") {
-        alert("Please enter a valid first name and last name.");
+        alert("Please, enter a valid first name and last name.\nExemple: Jonh Smith");
+        return false;
+    }
+
+    if(!namePattern.test(fName)){
+        alert("Please, enter a valid first name and last name.\nExemple: Jonh Smith");
         return false;
     }
 
     // Test email validation
     if(!(emailPattern.test(mail))){
-        alert("Please enter a valid email address.  ");
+        alert("Please, enter a valid email address.");
         return false;
     }
 
     // Check the query selector
     if(querys === null){
-        alert("Please select a query type.");
+        alert("Please, select a query type.");
         return false;
     }
 
     // Confirm checkBox
     if(checkBox === null){
-        alert("Please select the chackbox.");
+        alert("Please, select the chackbox.");
         return false;
     }
 
